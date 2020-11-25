e5b412d784a395203c730a6b38ed56cd4a47251f
zhangzh18 <zhang-zhihuang@foxmail.com>
2020-11-25 19:41:32 +0800

    update ignore

--- QXWZ_ROS_WS/build/qxwz_rtcm_service/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/zzh/ROS/X.OTHERS/QXWZ_ROS_WS/src/qxwz_rtcm_service/include".split(';') if "/home/zzh/ROS/X.OTHERS/QXWZ_ROS_WS/src/qxwz_rtcm_service/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "qxwz_rtcm_service"
-PROJECT_SPACE_DIR = "/home/zzh/ROS/X.OTHERS/QXWZ_ROS_WS/devel"
-PROJECT_VERSION = "0.0.0"
